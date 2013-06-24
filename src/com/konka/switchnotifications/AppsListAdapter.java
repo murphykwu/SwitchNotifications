a9b06df64b2b84b1cd9468fd4734df462b0f6d92
murphykwu <murphyk1@126.com>
2013-06-24 20:25:49 +0800

    add 增加中文文件，改变为系统程序

--- src/com/konka/switchnotifications/AppsListAdapter.java
+++ src/com/konka/switchnotifications/AppsListAdapter.java
@@ -1,7 +1,13 @@
 package com.konka.switchnotifications;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
 import com.konka.switchnotifications.widget.SwitchButton;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,28 +16,58 @@ import android.widget.TextView;
 
 public class AppsListAdapter extends BaseAdapter {
 
+	private Context mContext;
+	private LayoutInflater mLayoutInflater;
+	private List<AppInfo> mData = new ArrayList<AppInfo>();
+	
+	public AppsListAdapter(Context context, List<AppInfo> data)
+	{
+		mContext = context;
+		mData = data;
+		mLayoutInflater = LayoutInflater.from(context);
+	}
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return mData.size();
 	}
 
 	@Override
 	public Object getItem(int arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		return mData.get(arg0);
 	}
 
 	@Override
 	public long getItemId(int arg0) {
 		// TODO Auto-generated method stub
-		return 0;
+		return arg0;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		ViewHolder viewHoler = new ViewHolder();
+		if(convertView == null)
+		{
+			convertView = mLayoutInflater.inflate(R.layout.app_list_item, null);
+			viewHoler.iv_app = (ImageView)convertView.findViewById(R.id.iv_app_icon);
+			viewHoler.tv_app = (TextView)convertView.findViewById(R.id.tv_app_name);
+			viewHoler.sbtn_app = (SwitchButton)convertView.findViewById(R.id.sb_switch);
+			viewHoler.sbtn_app.setClickable(false);
+			convertView.setTag(viewHoler);
+		}else
+		{
+			viewHoler = (ViewHolder)convertView.getTag();
+		}
+		
+		//设置图标、名称、消息通知可否
+		viewHoler.iv_app.setImageDrawable(mData.get(position).appIcon);
+		viewHoler.tv_app.setText(mData.get(position).appName);
+		viewHoler.sbtn_app.setChecked(mData.get(position).appCanNotification);
+		
+		
+		return convertView;
 	}
 
 	
--- src/com/konka/switchnotifications/MainActivity.java
+++ src/com/konka/switchnotifications/MainActivity.java
@@ -3,21 +3,28 @@ package com.konka.switchnotifications;
 //import android.app.INotificationManager;
 import java.util.ArrayList;
 import java.util.List;
-import android.os.Bundle;
-import android.os.ServiceManager;
+
 import android.app.Activity;
 import android.app.INotificationManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 //	private ApplicationsState mApplicationsState;
-	PackageManager mPackageManager;
-	List<PackageInfo> mPackageInfoList;
-	ArrayList<AppInfo> mAppsList;//存放所有安装程序的数据
+	private PackageManager mPackageManager;
+	private List<PackageInfo> mPackageInfoList;
+	private ArrayList<AppInfo> mAppsList;//存放所有安装程序的数据
+	private ListView lv_apps;
+	private AppsListAdapter mAppsAdp;
+	private View mLayoutContainerLoading;
 		
 
 	@Override
@@ -28,12 +35,23 @@ public class MainActivity extends Activity {
 		mPackageManager = this.getPackageManager();
 		mPackageInfoList = mPackageManager.getInstalledPackages(0);
 		mAppsList = new ArrayList<AppInfo>();
-		
+		lv_apps = (ListView)this.findViewById(R.id.lv_apps);
+		mLayoutContainerLoading = (View)this.findViewById(R.id.loading_container);
+		mLayoutContainerLoading.setVisibility(View.VISIBLE);
+		lv_apps.setVisibility(View.INVISIBLE);
 		initAppsList();
+//		mAppsAdp = new AppsListAdapter(this.getApplicationContext(), mAppsList);///////
+//		lv_apps.setAdapter(mAppsAdp);
 	}
 	
+	/**
+	 * 在程序刚启动的时候，初始化目标应用程序的各种数据
+	 * 名称、图标、是否可以推送消息。因为有可能用户在管理应用程序这个
+	 * 界面对是否可以推送消息进行了修改。
+	 */
 	private void initAppsList()
 	{
+		//为了更友好的显示程序，要求在初始化列表的时候显示一个滚动条来提示用户等待
 		INotificationManager nm = INotificationManager.Stub.asInterface(
 				ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 		for(int i = 0; i < mPackageInfoList.size(); i ++)
@@ -50,10 +68,20 @@ public class MainActivity extends Activity {
 				tmpInfo.versionCode = packageInfo.versionCode;
 				tmpInfo.appIcon = packageInfo.applicationInfo.loadIcon(mPackageManager);
 				
+				//获取当前应用通知状态，是否可以发送通知
+				try {
+					tmpInfo.appCanNotification = nm.areNotificationsEnabledForPackage(tmpInfo.packageName);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					tmpInfo.appCanNotification = false;
+				}
 				
 				mAppsList.add(tmpInfo);
 			}
 		}
+		mAppsAdp = new AppsListAdapter(this.getApplicationContext(), mAppsList);///////
+		lv_apps.setAdapter(mAppsAdp);
 	}
 
 	@Override
